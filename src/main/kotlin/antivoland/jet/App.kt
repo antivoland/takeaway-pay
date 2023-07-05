bd6d2e0e812a1a87ab9a63f3cb6375291c017c3a
Vladimir Pavlovskii <antivoland@gmail.com>
2023-07-05 16:51:56 +0300

    setup Kotlin and Spring Boot

---
+++ src/main/kotlin/antivoland/jet/App.kt
@@ -0,0 +1,12 @@
+package antivoland.jet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class App
+
+fun main(args: Array<String>) {
+    runApplication<App>(*args)
+    println("Hello, Kotlin!")
+}
